637ad877af5aa6aca41b89ebc4b2dfb1d03c2e43
Joel Mitchell <joel.mitchell@uberdigital.com>
2014-06-10 13:24:56 +0100

    a

--- TestCoreModule/StringExtensions.cs
+++ TestCoreModule/StringExtensions.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CuttingEdge.Conditions;
 
 namespace TestCoreModule
 {
     public static class StringExtensions
     {
-        public static string AddJoel { get; set; }
+        public static string AddJoel(this string s)
+        {
+            Condition.Requires(s).IsNotNull();
+            return s += "Joel";
+        }
     }
 }
